d2f96b8043576840a504a21925a9e2d4ae5bc01d
Václav Seidl <vaclav.seidl@inventing-animals.com>
2026-04-08 14:24:37 +0300

    Added overlay to the Menu control

--- src/Ink.UI/Controls/Menu/MenuItem.cs
+++ src/Ink.UI/Controls/Menu/MenuItem.cs
@@ -1,4 +1,7 @@
 using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.LogicalTree;
 
 namespace Ink.UI.Controls;
 
@@ -6,6 +9,9 @@ public class MenuItem : Avalonia.Controls.MenuItem
 {
     public static readonly StyledProperty<ButtonVariant> VariantProperty =
         AvaloniaProperty.Register<MenuItem, ButtonVariant>(nameof(Variant), ButtonVariant.Secondary);
+
+    private Popup? _popup;
+    private bool _overlayVisible;
 
     static MenuItem()
     {
@@ -23,6 +29,34 @@ public class MenuItem : Avalonia.Controls.MenuItem
         set => SetValue(VariantProperty, value);
     }
 
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+
+        _popup = e.NameScope.Find("PART_Popup") as Popup;
+        if (_popup is not null)
+            _popup.OverlayDismissEventPassThrough = System.OperatingSystem.IsBrowser();
+    }
+
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+
+        if (change.Property == IsSubMenuOpenProperty && IsTopLevelTrigger())
+        {
+            if (change.GetNewValue<bool>())
+                ShowOverlay();
+            else
+                HideOverlay();
+        }
+    }
+
+    protected override void OnDetachedFromLogicalTree(LogicalTreeAttachmentEventArgs e)
+    {
+        HideOverlay();
+        base.OnDetachedFromLogicalTree(e);
+    }
+
     private static void OnVariantChanged(MenuItem menuItem, AvaloniaPropertyChangedEventArgs e)
     {
         menuItem.ApplyVariantClass(e.GetNewValue<ButtonVariant>());
@@ -31,5 +65,25 @@ public class MenuItem : Avalonia.Controls.MenuItem
     private void ApplyVariantClass(ButtonVariant variant)
     {
         ButtonVariantClassHelper.Apply(this, variant);
+    }
+
+    private bool IsTopLevelTrigger() => Parent is Menu;
+
+    private void ShowOverlay()
+    {
+        if (_overlayVisible)
+            return;
+
+        InkOverlay.Show(this, () => IsSubMenuOpen = false);
+        _overlayVisible = true;
+    }
+
+    private void HideOverlay()
+    {
+        if (!_overlayVisible)
+            return;
+
+        InkOverlay.Hide(this);
+        _overlayVisible = false;
     }
 }
